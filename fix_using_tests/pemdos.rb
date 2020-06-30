b554d91730e24bb98e9839641eb6a1c295af910c
Jzavier Timm <jztimm@gmail.com>
2020-06-30 00:55:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-   new_str = 10.times {puts "#{string[0]}\n"}
+   new_str = 10.times {puts "s\n"}
   else
     puts new_str + string
   end
