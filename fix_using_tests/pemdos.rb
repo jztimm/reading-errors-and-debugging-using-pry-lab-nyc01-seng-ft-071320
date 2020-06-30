bf77cb95a6a5b823afaae5e48cf8b2fc393fa259
Jzavier Timm <jztimm@gmail.com>
2020-06-30 00:43:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,12 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
     "s".times(10) + string
+    binding.pry
+    string
   else
     string
   end
